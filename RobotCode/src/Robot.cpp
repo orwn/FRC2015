840ce5758cf2ffe6dd9bfc6390ff3774fcd02c6a
orwn <oror.wn@gmail.com>
2014-08-27 12:17:04 +0300

    Update Robot.cpp

--- RobotCode/src/Robot.cpp
+++ RobotCode/src/Robot.cpp
@@ -1,33 +1,39 @@
 #include "WPILib.h"
+#include "Commands/Command.h"
+#include "Commands/ExampleCommand.h"
+#include "CommandBase.h"
 
 class Robot: public IterativeRobot
 {
 private:
+	Command * autonomousCommand;
 	LiveWindow *lw;
 
 	void RobotInit()
 	{
+		CommandBase::Init();
+		autonomousCommand = new ExampleCommand();
 		lw = LiveWindow::GetInstance();
 	}
 
 	void AutonomousInit()
 	{
-
+		autonomousCommand->Start();
 	}
 
 	void AutonomousPeriodic()
 	{
-
+		Scheduler::GetInstance()->Run();
 	}
 
 	void TeleopInit()
 	{
-
+		autonomousCommand->Cancel();
 	}
 
 	void TeleopPeriodic()
 	{
-
+		Scheduler::GetInstance()->Run();
 	}
 
 	void TestPeriodic()
